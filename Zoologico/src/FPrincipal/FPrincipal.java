8b226b4a53fd49ddabbd78f42326b40f7c4160c9
María José Amorocho <123780212+MariaAmor8@users.noreply.github.com>
2023-11-30 20:08:52 -0500

    zoologico

---
+++ Zoologico/src/FPrincipal/FPrincipal.java
@@ -0,0 +1,50 @@
+package FPrincipal;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class FPrincipal extends JFrame{
+	JTextField txtAnimal;
+	JTextField txtEdad;
+	JComboBox CBhabitat;
+	
+	public void ejecutar() {
+		this.setPreferredSize(new Dimension(350,250));
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+		this.setLayout(new GridLayout(4,2));
+		this.add(new JLabel("Nombre del animal"));
+		this.txtAnimal= new JTextField();
+		this.add(txtAnimal);
+		
+		this.add(new JLabel("Edad del animal"));
+		this.txtEdad= new JTextField();
+		this.add(txtEdad);
+		
+		this.add(new JLabel("Habitad del animal"));
+		String[] habitats = {"desierto", "selva", "polo norte"};
+		this.CBhabitat = new JComboBox(habitats);
+		this.CBhabitat.setBackground(Color.white);
+		this.add(this.CBhabitat);
+		
+		JButton bEnviar = new JButton("Registrar animal");
+		this.add(bEnviar);
+		this.pack();
+	}
+	
+	public static void main(String[] args) {
+		FPrincipal principal = new FPrincipal();
+		principal.ejecutar();
+	}
+	
+
+}
